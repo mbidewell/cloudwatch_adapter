05b94ec9a8cabb8a90275ce65475a47e7dac638c
Mark Bidewell <mbidewel@gmail.com>
2019-08-06 19:56:07 -0400

    Initial Cloudwatch adapter

--- server.go
+++ server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"math"
+	"time"
 	"net/http"
 	"io/ioutil"
 
-	"github.com/gocql/gocql"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/snappy"
@@ -13,7 +17,7 @@ import (
 	"github.com/prometheus/prometheus/prompb"
 )
 
-func handleWrite(w http.ResponseWriter, r *http.Request, session *gocql.Session) {
+func handleWrite(w http.ResponseWriter, r *http.Request, c *cloudwatch.CloudWatch) {
 	compressed, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.With("err", err).Error("Failed to read body")
@@ -35,27 +39,55 @@ func handleWrite(w http.ResponseWriter, r *http.Request, session *gocql.Session)
 		return
 	}
 
+	var metrics []*cloudwatch.MetricDatum
 	for _, ts := range req.Timeseries {
-		metric := make(model.Metric, len(ts.Labels))
+
 		metric_name := ""
+		instance := ""
+		var dimensions []*cloudwatch.Dimension
 
 		for _, l := range ts.Labels {
 			if l.Name == model.MetricNameLabel {
 				metric_name = l.Value
-			} else {
-				metric[model.LabelName(l.Name)] = model.LabelValue(l.Value)
+			} else if l.Name == "instance" {
+				instance = l.Value
 			}
 		}
-
+		dimensions = append(dimensions, &cloudwatch.Dimension{
+			Name: aws.String("instance"),
+			Value: aws.String(instance),
+		})
 		for _, s := range ts.Samples {
-			err := session.Query("insert into metrics(timestamp,name,labels_hash, labels, value) values(?,?,?,?,?)",
-														s.Timestamp,
-														metric_name,
-														metric.Fingerprint().String(),
-														metric,
-														s.Value).Exec()
+			if(!math.IsNaN(s.Value)) {
+				metrics = append(metrics, &cloudwatch.MetricDatum {
+					MetricName: aws.String(metric_name),
+					Value: aws.Float64(s.Value),
+					Timestamp: aws.Time(time.Unix(s.Timestamp / 1000, 0)),
+					Dimensions: dimensions,
+				})
+			}
+			if len(metrics) == 20 {
+				_, err := c.PutMetricData(&cloudwatch.PutMetricDataInput {
+					MetricData: metrics,
+					Namespace: aws.String("Prometheus"),
+				})
+
+				if  err != nil {
+					log.With("err", err).Error("Cannot write Samples")
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+				metrics = nil
+				time.Sleep(1000 * time.Millisecond)
+			}
+		}
+		if metrics != nil {
+			_, err := c.PutMetricData(&cloudwatch.PutMetricDataInput {
+				MetricData: metrics,
+				Namespace: aws.String("Prometheus"),
+			})
+
 			if  err != nil {
-				log.With("err", err).Error("Cannot write Sample")
+				log.With("err", err).Error("Cannot write Samples")
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 			}
 		}
@@ -64,17 +96,19 @@ func handleWrite(w http.ResponseWriter, r *http.Request, session *gocql.Session)
 
 
 func main() {
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "prometheus"
-	session, err := cluster.CreateSession()
+	sess, err := session.NewSession(&aws.Config{
+	    Region: aws.String("us-east-1")},
+	)
 
 	if err != nil {
-		log.With("err", err).Error("Cannot Connect to DB")
+		log.With("err", err).Error("Cannot Create Session")
 		return
 	}
+	c := cloudwatch.New(sess)
+
 
 	http.HandleFunc("/write", func(w http.ResponseWriter, r *http.Request) {
-		handleWrite(w, r, session)
+		handleWrite(w, r, c)
 	})
 
 	log.Fatal(http.ListenAndServe(":9201", nil))
